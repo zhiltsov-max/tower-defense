ce2c50750e3b92900818b8fe18682c1fcadc4aa3
zhiltsov-max <zhiltsov.max35@gmail.com>
2018-06-08 10:29:55 +0300

    Project exports reworked

--- cmake/Scripts/ProjectExport.cmake
+++ cmake/Scripts/ProjectExport.cmake
@@ -1,44 +1,58 @@
-# Create package-config files :
-#  - <projectname>ConfigVersion.cmake
-#  - <projectname>Config.cmake
-# They are installed in lib/cmake/<projectname>.
+# Script to generage configuration files of the project.
+# These files contain information about targets, package version and variables.
 #
-# Required variables :
+# Required variables:
 #  - VERSION
 #  - PROJECT_NAME
 #
+# Required files:
+#  - <project>/cmake/${PROJECT_NAME}Config.cmake.in
+#
+# Target export requirements:
+#  - ${PROJECT_NAME}Targets - should be used when installing target
+#
+# Outputs:
+#  - <projectname>ConfigVersion.cmake
+#  - <projectname>Config.cmake
+#  - <projectname>Targets.cmake
+# They are installed in lib/cmake/<projectname>.
+# The project targets are installed under the ${PROJECT_NAME}:: namespace.
+#
 
-# Include needed for 'write_basic_package_version_file'
 include(CMakePackageConfigHelpers)
 
+# Destination for config files
+set(CONFIG_INSTALL_DIR lib/cmake/${PROJECT_NAME})
+
+# Add exported targets
+export(EXPORT ${PROJECT_NAME}Targets
+    FILE ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}/${PROJECT_NAME}Targets.cmake
+    NAMESPACE ${PROJECT_NAME}::
+)
+
+# Create config file
+configure_package_config_file(
+    ${PROJECT_SOURCE_DIR}/cmake/${PROJECT_NAME}Config.cmake.in
+    ${PROJECT_BINARY_DIR}/cmake/${PROJECT_NAME}Config.cmake
+    INSTALL_DESTINATION ${CONFIG_INSTALL_DIR}
+)
+
+# Create version file
 write_basic_package_version_file(
-    "${CMAKE_CURRENT_BINARY_DIR}/cmake/${PROJECT_NAME}ConfigVersion.cmake"
-    VERSION ${VERSION}
+    ${PROJECT_BINARY_DIR}/cmake/${PROJECT_NAME}ConfigVersion.cmake
+    VERSION ${YART_VERSION}
     COMPATIBILITY AnyNewerVersion
 )
 
-configure_file(cmake/${PROJECT_NAME}Config.cmake
-    "${CMAKE_CURRENT_BINARY_DIR}/cmake/${PROJECT_NAME}Config.cmake"
-    COPYONLY
+# Targets installation
+install(EXPORT ${PROJECT_NAME}Targets
+  FILE ${PROJECT_NAME}Targets.cmake
+  NAMESPACE ${PROJECT_NAME}::
+  DESTINATION ${CONFIG_INSTALL_DIR}
 )
 
-# Destination
-set(config_install_dir lib/cmake/${PROJECT_NAME})
-
-# Config installation
-#   * <prefix>/lib/cmake/<project>/<project>Targets.cmake
-install(
-    EXPORT ${PROJECT_NAME}Targets
-    DESTINATION ${config_install_dir}
-)
-
-# Config installation
-#   * <prefix>/lib/cmake/<project>/<project>Config.cmake
-#   * <prefix>/lib/cmake/<project>/<project>ConfigVersion.cmake
-install(
-    FILES
-        cmake/${PROJECT_NAME}Config.cmake
-        "${CMAKE_CURRENT_BINARY_DIR}/cmake/${PROJECT_NAME}ConfigVersion.cmake"
-    DESTINATION ${config_install_dir}
-    COMPONENT devel
-)
+# Config files installation
+install(FILES
+        ${PROJECT_BINARY_DIR}/cmake/${PROJECT_NAME}Config.cmake
+        ${PROJECT_BINARY_DIR}/cmake/${PROJECT_NAME}ConfigVersion.cmake
+    DESTINATION ${CONFIG_INSTALL_DIR})
