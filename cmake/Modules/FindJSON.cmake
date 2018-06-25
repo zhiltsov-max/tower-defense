42fd9892f0047966c0c53f70b65a59438eaaa072
zhiltsov-max <zhiltsov.max35@gmail.com>
2018-06-25 09:48:26 +0300

    GE: modernized 3rdparty dependencies

--- cmake/Modules/FindJSON.cmake
+++ cmake/Modules/FindJSON.cmake
@@ -8,6 +8,9 @@
 # - JSON_LIBRARIES - empty value
 # - JSON_DEFINITIONS - compiler definitions required to use the library
 #
+# Script creates the following imported targets:
+# - json::json
+#
 
 find_path(JSON_INCLUDE_DIR json.hpp
     HINTS ${JSON_ROOT}
@@ -18,5 +21,13 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(JSON
     REQUIRED_VARS JSON_INCLUDE_DIR)
 
-set(JSON_LIBRARIES ${JSON_LIBRARY})
-set(JSON_INCLUDE_DIRS ${JSON_INCLUDE_DIR})
+if (JSON_FOUND AND NOT TARGET json::json)
+    set(JSON_LIBRARIES ${JSON_LIBRARY})
+    set(JSON_INCLUDE_DIRS ${JSON_INCLUDE_DIR})
+
+    add_library(json::json INTERFACE IMPORTED)
+    set_property(TARGET json::json
+        PROPERTY INTERFACE_INCLUDE_DIRECTORIES
+            ${JSON_INCLUDE_DIRS}
+        )
+endif()
--- cmake/Modules/FindLUAPP.cmake
+++ cmake/Modules/FindLUAPP.cmake
@@ -8,6 +8,9 @@
 # - LUAPP_LIBRARIES - empty value
 # - LUAPP_DEFINITIONS - compiler definitions required to use the library
 #
+# Script creates the following imported targets:
+# - luapp::luapp
+#
 # The LUAPP_COMPATIBILITY variable might be set to specify used LUA version.
 # The accessible values are "5.1", "5.2" and "5.3"
 
@@ -16,13 +19,13 @@ if (NOT DEFINED LUAPP_COMPATIBILITY)
 endif()
 
 if (LUAPP_COMPATIBILITY EQUAL "5.1")
-    find_package(LUA 5.1)
+    find_package(LUA 5.1 REQUIRED)
     set(LUAPP_DEFINITIONS -DLUAPP_COMPATIBILITY_V51)
 elseif(LUAPP_COMPATIBILITY EQUAL "5.2")
-    find_package(LUA 5.2)
+    find_package(LUA 5.2 REQUIRED)
     set(LUAPP_DEFINITIONS -DLUAPP_COMPATIBILITY_V52)
 elseif(LUAPP_COMPATIBILITY EQUAL "5.3")
-    find_package(LUA 5.3)
+    find_package(LUA 5.3 REQUIRED)
     set(LUAPP_DEFINITIONS -DLUAPP_COMPATIBILITY_V53)
 else()
     message(FATAL_ERROR "LUAPP supports only 5.1, 5.2 and 5.3 LUA versions.")
@@ -37,5 +40,22 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(LUAPP
     REQUIRED_VARS LUAPP_INCLUDE_DIR)
 
-set(LUAPP_LIBRARIES ${LUAPP_LIBRARY})
-set(LUAPP_INCLUDE_DIRS ${LUAPP_INCLUDE_DIR})
+if (LUAPP_FOUND AND NOT TARGET luapp::luapp)
+    set(LUAPP_LIBRARIES ${LUAPP_LIBRARY})
+    set(LUAPP_INCLUDE_DIRS ${LUAPP_INCLUDE_DIR})
+
+    add_library(luapp::luapp INTERFACE IMPORTED)
+    set_property(TARGET luapp::luapp
+        PROPERTY INTERFACE_INCLUDE_DIRECTORIES
+            ${LUA_INCLUDE_DIRS}
+            ${LUAPP_INCLUDE_DIRS}
+        )
+    set_property(TARGET luapp::luapp
+        PROPERTY INTERFACE_LINK_LIBRARIES
+            ${LUA_LIBRARIES}
+        )
+    set_property(TARGET luapp::luapp
+        PROPERTY INTERFACE_COMPILE_DEFINITIONS
+            ${LUAPP_DEFINITIONS}
+        )
+endif()
